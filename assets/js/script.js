9f80a8f96260eedfd47476509ddd5944ed74ca3d
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-01-01 13:20:57 +0000

    Add start quiz function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
-function startQuiz() {
+let startButton = document.getElementById('start-btn')
+let quizArea = document.getElementById('question-area')
 
+startButton.addEventListener('click', startQuiz)
+
+/**
+ * Function starts the quiz once user clicks
+ * the start quiz button
+ */
+
+function startQuiz() {
+  console.log('starting')
+  startButton.classList.add('hide')
+  quizArea.classList.remove('hide')
+  nextQuestion()
 }
 
 function nextQuestion() {
