3827141289ed882653ac09203c9fac52bdeef5f0
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-01-02 23:22:10 +0000

    Add javascript code for 1st question to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 let startButton = document.getElementById('start-btn')
+let nextButton = document.getElementById('next-btn')
 let quizArea = document.getElementById('question-area')
+let questionText = document.getElementById('question')
+let answer1 = document.getElementById('answer1')
+let answer2 = document.getElementById('answer2')
+let answer3 = document.getElementById('answer3')
+
+let currentQuestion = 0;
 
 startButton.addEventListener('click', startQuiz)
 
@@ -12,11 +19,17 @@ function startQuiz() {
   console.log('starting')
   startButton.classList.add('hide')
   quizArea.classList.remove('hide')
-  nextQuestion()
+  nextButton.classList.remove('hide')
+  questionText.innerText = questions[currentQuestion].question
+  answer1.innerText = questions[currentQuestion].answer1
+  answer2.innerText = questions[currentQuestion].answer2
+  answer3.innerText = questions[currentQuestion].answer3
+
+
 }
 
 function nextQuestion() {
-
+  
 }
 
 function selectAnswewr() {
@@ -26,3 +39,20 @@ function selectAnswewr() {
 function submitScore() {
 
 }
+
+const questions = [
+  {
+    question: 'What is 2 * 5',
+    answer1: '25',
+    answer2:'10',
+    answer3: '5',
+    correct: '10'
+  },
+  {
+    question: 'What is 2 * 8',
+    answer1: '16',
+    answer2:'8',
+    answer3: '24',
+    correct: '1'
+  }
+]
