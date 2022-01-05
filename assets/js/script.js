91ff5030d02728c151d2c705e0c01faefe58ddf4
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-01-05 19:35:52 +0000

    Add checkAnswer function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 const startButton = document.getElementById('start-btn');
-const nextButton = document.getElementById('next-btn');
+const restartButton = document.getElementById('restart-btn');
 const quizArea = document.getElementById('question-area');
 const questionText = document.getElementById('question');
 const choiceA = document.getElementById('A');
 const choiceB = document.getElementById('B');
 const choiceC = document.getElementById('C');
+let userScore = document.getElementById('user-score')
 
 const questions = [
   {
@@ -13,8 +14,8 @@ const questions = [
     choiceB:'10',
     choiceC: '5',
     correct: 'B'
-  },
-  {
+   },
+   {
     question: 'What is 2 x 8?',
     choiceA: '16',
     choiceB:'8',
@@ -28,6 +29,7 @@ const questions = [
     choiceC: '24',
     correct: 'A'
   }
+  
 ]
 
 const lastQuestion = questions.length -1;
@@ -35,14 +37,22 @@ let currentQuestion = 0;
 let score = 0;
 
 function nextQuestion() {
-  let q = questions[currentQuestion]
-  questionText.innerText = q.question
-  choiceA.innerText = q.choiceA
-  choiceB.innerText = q.choiceB
-  choiceC.innerText = q.choiceC
+  if (currentQuestion > lastQuestion) {
+    console.log('end')
+  } else {
+    let q = questions[currentQuestion]
+    questionText.innerText = q.question
+    choiceA.innerText = q.choiceA
+    choiceB.innerText = q.choiceB
+    choiceC.innerText = q.choiceC
+  }
+
+  userScore.innerHTML = `Total score: ` + `${score}`;
 }
 
 startButton.addEventListener('click', startQuiz)
+restartButton.addEventListener('click', startQuiz)
+
 
 /**
  * Function starts the quiz once user clicks
@@ -53,15 +63,37 @@ function startQuiz() {
   nextQuestion()
   console.log('starting')
   startButton.classList.add('hide')
+  restartButton.classList.add('hide')
   quizArea.classList.remove('hide')
-  nextButton.classList.remove('hide')
-  
-  
+    
 }
 
 
 
-function checkAnswer(answer) {}
+function checkAnswer(answer) {
+  if(answer === questions[currentQuestion].correct) {
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Correct',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    score++
+    currentQuestion++
+    nextQuestion()
+  } else {
+    Swal.fire({
+      position: 'center',
+      icon: 'error',
+      title: 'Wrong',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    currentQuestion++
+    nextQuestion()
+  }
+}
 
 
 
